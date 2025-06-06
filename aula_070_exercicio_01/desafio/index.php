7eff3a464eda0615cb9f1056412097f68ccff89b
Diego Santana <3c.santana@gmail.com>
2025-06-06 11:35:35 -0300

    feat: Implementa a lógica para exibir os erros no formulário

--- aula_070_exercicio_01/desafio/index.php
+++ aula_070_exercicio_01/desafio/index.php
@@ -1,3 +1,36 @@
+<?php
+    // Inicia a sessão para acessar valores armazenados anteriormente
+    session_start();
+
+    // Inicializa o array que conterá os dados dos campos
+    $inputs = [];
+
+    // Se houver dados salvos na sessao carregamos e limpamos
+    if(isset($_SESSION['inputs'])) {
+        $inputs = $_SESSION['inputs'];
+        unset($_SESSION['inputs']); // Limpamos para não reutilizar em outro acesso
+    }
+
+    // Função auxiliar para exibir o valor digitado anteriormente em um campo
+    function showValue(string $field): string {
+        global $inputs;
+        // Se existir um valor para o campo, retornamos. Senão, uma string vazia
+        return $inputs[$field]['value'] ?? '';
+    }
+
+    // Função auxiliar para exibir mensagens de erro específicas do campo
+    function showError(string $field): string {
+        global $inputs;
+        // Se houver erro para o campo, retornamos um <div> com a mensagem
+        if(!empty($inputs[$field]['error'])) {
+            return "<div class='error'>{$inputs[$field]['error']}</div>";
+        } else {
+            return ''; // Caso contrário, não mostramos nada
+        }
+    }
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,6 +43,11 @@
         .wrapper {
             width: 600px;
             margin: 50px auto;
+        }
+
+        .error {
+            color: red;
+            margin-bottom: 5px;
         }
 
         input[type=text] {
@@ -25,9 +63,12 @@
     <div class="wrapper">
         <form action="tratamento.php" method="post">
             <label>Valor 1</label>
-            <input type="text" name="text_valor_1">
+            <input type="text" name="text_valor_1" value="<?= showValue('text_valor_1') ?>">
+            <?= showError('text_valor_1') ?> <!-- Mostra erro, se houver -->
             <label>Valor 2</label>
-            <input type="text" name="text_valor_2">
+            <input type="text" name="text_valor_2" value="<?= showValue('text_valor_2') ?>">
+            <?= showError('text_valor_2') ?> <!-- Mostra erro, se houver -->
+            <!-- Botão para enviar o formulário -->
             <input type="submit" value="Executar">
         </form>
     </div>
