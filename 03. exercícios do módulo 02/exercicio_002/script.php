3e835c1eefd9ed451ea739718a8ceb068c5d66ab
Diego Santana <3c.santana@gmail.com>
2025-07-05 09:21:24 -0300

    feat: Exercício 02

---
+++ 03. exercícios do módulo 02/exercicio_002/script.php
@@ -0,0 +1,16 @@
+<?php
+    $firstName = 'Bob';
+    $lastName = 'Brown';
+    $fullName = "$firstName $lastName";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 02</title>
+</head>
+<body>
+    <h1>Nome completo: <?= $fullName ?></h1>
+</body>
+</html>
