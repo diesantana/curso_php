f52cd53715c94b16d163430f829c3368fe811ee1
Diego Santana <3c.santana@gmail.com>
2025-07-06 09:55:26 -0300

    feat: Exercício 03

---
+++ 03. exercícios do módulo 02/exercicio_003/index.php
@@ -0,0 +1,16 @@
+<?php
+    $value1 = 15;
+    $value2 = 37;
+    $result = $value1 + $value2;
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 03</title>
+</head>
+<body>
+    <h1><?= "$value1 + $value2 = $result" ?></h1>
+</body>
+</html>
