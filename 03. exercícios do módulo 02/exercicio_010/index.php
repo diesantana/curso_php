db79d46a771dd2d46d0210e8cdbc4e34ea841706
Diego Santana <3c.santana@gmail.com>
2025-07-15 18:23:50 -0300

    feat: Exercício 10

---
+++ 03. exercícios do módulo 02/exercicio_010/index.php
@@ -0,0 +1,40 @@
+<?php
+    // Gera uma quantidade x ($amountValues) de valores aleatórios únicos.
+    // O retorno é um array contendo os valores randomicos
+    function generateUniqueValues($amountValues) {
+        $values = [];
+
+        while(count($values) < 20) {
+            $currentValue = rand(1, 100);
+            if(in_array($currentValue, $values)) continue; 
+            // se o valor já existir no array ignora o cód abaixo é ignorado
+            $values[] = $currentValue;
+        }
+
+        return $values;
+    }
+
+    $values = generateUniqueValues(20);
+    // ordenando o array
+    sort($values);
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 10</title>
+</head>
+
+<body>
+    <h1>Lista de valores:</h1>
+    <ul>
+        <?php foreach ($values as $value): ?>
+        <li><?= $value ?></li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+
+</html>
