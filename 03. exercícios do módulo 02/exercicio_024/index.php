48edee5ade10c9a1b1856ad5285bcaff5ba76683
Diego Santana <3c.santana@gmail.com>
2025-08-03 18:29:54 -0300

    feat: Implementa a lógica de tratamento para o form

--- 03. exercícios do módulo 02/exercicio_024/index.php
+++ 03. exercícios do módulo 02/exercicio_024/index.php
@@ -1,6 +1,28 @@
+<?php
+
+$name = '';
+$lastname = '';
+$error = '';
+$message = '';
+
+// Verificar se o formulário foi submetido corretamente.
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Captura os valores enviados pelo formulário
+    // caso não existam dados, atribui uma string vazia. 
+    $name = trim($_POST['text_nome']) ?? '';
+    $lastname = trim($_POST['text_sobrenome']) ?? '';
+    
+    // Validação dos campos
+    if(empty($name) || empty($lastname)) {
+        $error = 'Os campos nome e sobrenome são obrigatórios';
+    } else {
+        $message = "Olá $name $lastname";
+    }    
+}
+
+?>
 <!DOCTYPE html>
 <html lang="pt">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,7 +30,6 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-
 <body>
 
     <div class="container mt-5">
@@ -26,11 +47,18 @@
                     </div>
                 </form>
             </div>
+            <div class="col-12 text-center">
+                <?php if(!empty($error)):?>
+                    <p class="text-danger"><?= $error?></p>
+                <?php endif;?>
+                <?php if(!empty($message)):?>
+                    <p class="text-success"><?= $message?></p>
+                <?php endif;?>
+            </div>
         </div>
     </div>
 
-    <script src="../assets/bootstrap/bootstrap.bundle.min.js"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
-
 </html>
