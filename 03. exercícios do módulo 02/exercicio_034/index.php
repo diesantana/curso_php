8e5c003ad41838fb3ad9b646ff47eb82bab6e114
Diego Santana <3c.santana@gmail.com>
2025-08-17 09:14:17 -0300

    feat: Implementa a lógica de gerar iniciais

--- 03. exercícios do módulo 02/exercicio_034/index.php
+++ 03. exercícios do módulo 02/exercicio_034/index.php
@@ -11,7 +11,17 @@
  * Dado "Fernanda Costa Pereira" retornar "F.C.P."
  */
 function gerarIniciais($nomeCompleto) {
-    // Seu código aqui (3-5 linhas)
+    // Divide a string em um array onde cada item do array é uma parte do nome
+    $dividedName = explode(' ', strtoupper($nomeCompleto));
+    $initials = '';
+
+    // Itera sobre o array com partes do nome, para pegar a primeira letra
+    // Monta as iniciais com a primeira letra de cada item do array
+    foreach($dividedName as $partName) {
+        $initials .= $partName[0] . '.';
+    }
+
+    return $initials;
 }
 
 // ======================================
@@ -19,4 +29,6 @@ function gerarIniciais($nomeCompleto) {
 // ======================================
 echo "<h3>Teste - Gerador de Iniciais</h3>";
 echo gerarIniciais("Carlos Eduardo Martins") === "C.E.M." 
-    ? "✔ Correto" : "✘ Errado";
+? "✔ Correto" : "✘ Errado";
+
+// echo "<br>" . gerarIniciais("Fernanda Costa Pereira");
