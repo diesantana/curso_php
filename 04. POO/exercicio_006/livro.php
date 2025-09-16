31f888eeda8a75a465ac78aea540ed45c70b6256
Diego Santana <3c.santana@gmail.com>
2025-09-15 21:22:55 -0300

    feat: Implementa a classe Livro

---
+++ 04. POO/exercicio_006/livro.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+class Livro{
+    private string $title;
+    private string $author;
+    private int $pages;
+
+    public function __construct(string $title, string $author, int $pages) {
+        $this->title = $title;
+        $this->author = $author;
+        $this->pages = $pages;
+    }
+
+    public function getTitle():string {
+        return $this->title;
+    }
+
+    public function setTitle(string $newTitle):void {
+        $this->title = $newTitle;
+    }
+    
+    public function getAuthor():string {
+        return $this->author;
+    }
+
+    public function setAuthor(string $newAuthor):void {
+        $this->author = $newAuthor;
+    }
+    
+    public function getPages():int {
+        return $this->pages;
+    }
+
+    public function setPages(int $newPages):void {
+        $this->pages = $newPages;
+    }
+}
