6ce2accb4f51dd3663eae06767a9be6ba5a72fa3
Diego Santana <3c.santana@gmail.com>
2025-09-18 21:10:55 -0300

    feat: Implementa os métodos buscarPorAutor e contarPaginasTotais

--- 04. POO/exercicio_006/classes/biblioteca.php
+++ 04. POO/exercicio_006/classes/biblioteca.php
@@ -11,4 +11,22 @@ class Biblioteca{
     public function listarLivros():array {
         return $this->livros;
     }
+
+    public function buscarPorAutor(string $autor):array {
+        $livrosPorAutor = [];
+        foreach($this->livros as $livro) {
+            if($livro->getAuthor() == $autor) {
+                array_push($livrosPorAutor, $livro);
+            }
+        }
+        return $livrosPorAutor;
+    }
+
+    public function contarPaginasTotais():int {
+        $total = 0;
+        foreach($this->livros as $livro) {
+            $total += $livro->getPages();
+        }
+        return $total;
+    }
 }
