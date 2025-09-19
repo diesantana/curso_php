9d34b86bdb795cdac3ad83f1e608f925ef421e14
Diego Santana <3c.santana@gmail.com>
2025-09-18 21:11:32 -0300

    feat: Testa os métodos

--- 04. POO/exercicio_006/index.php
+++ 04. POO/exercicio_006/index.php
@@ -16,12 +16,14 @@ spl_autoload_register("carregarClasses");
 $livro1 = new Livro('1984', 'George Orwell', 328);
 $livro2 = new Livro('O Senhor dos Anéis', 'J.R.R. Tolkien', 1178);
 $livro3 = new Livro('Dom Casmurro', 'Machado de Assis', 256);
+$livro4 = new Livro('Livro Teste', 'J.R.R. Tolkien', 500);
 
 // Adicionando livros a biblioteca
 $biblioteca = new Biblioteca();
 $biblioteca->adicionarLivro($livro1);
 $biblioteca->adicionarLivro($livro2);
 $biblioteca->adicionarLivro($livro3);
+$biblioteca->adicionarLivro($livro4);
 
 // Listando os livros 
 $livros = $biblioteca->listarLivros();
@@ -29,3 +31,17 @@ foreach($livros as $livro) {
     echo $livro->details();
     echo '<br>';
 }
+
+// Livros por Autor
+echo '<hr>';
+echo 'Livros por Autor' . '<br>';
+$livrosPorAutor = $biblioteca->buscarPorAutor('J.R.R. Tolkien');
+foreach($livrosPorAutor as $livro) {
+    echo $livro->details();
+    echo '<br>';
+}
+
+// Total Páginas
+echo '<hr>';
+$totalPages = $biblioteca->contarPaginasTotais();
+echo "Total Páginas: $totalPages";
